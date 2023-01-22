1c98f93b1ebb3515747622077f36bdf39fe9bd7b
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-01-22 17:52:07 -0400

    :sparkles: feat: Pagina de Cad Aluno

--- cadastrar_aluno.php
+++ cadastrar_aluno.php
@@ -1,85 +1,3 @@
-<?php
-include('conexao.php');
-
-$erro = false;
-if (count($_POST) > 1) {
-
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $cpf = $_POST['cpf'];
-    $matricula = $_POST['matricula'];
-    $senha = $_POST['senha'];
-    $telefone = $_POST['telefone'];
-    $endereco = $_POST['endereco'];
-
-    if (empty($nome)) {
-        $erro = "Preencha o nome";
-    }
-    if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $erro = "Preencha o campo Email";
-    }
-    if (empty($cpf) || strlen($cpf) < 14 || !validarCpf($cpf)) {
-        $erro = "CPF Inválido. Tente novamente!";
-    }
-    $sqlQuery = false;
-    if (empty($matricula || strlen($matricula) > 25)) {
-        $erro = "Infomre uma matrícula válida!";
-    } else {
-        $sql_code = $mysqli->query("SELECT matricula FROM tbl_aluno WHERE matricula = '$matricula' LIMIT 1");
-        $sqlQuery = $sql_code->fetch_assoc();
-
-        if (!empty($sqlQuery['matricula'])) {
-            $erro = "Essa matrícula já está vinculada a um aluno registrado no sistema!";
-        } else {
-            $sql_code = $mysqli->query(
-                "SELECT matricula FROM tbl_matriculas WHERE matricula = '$matricula' LIMIT 1"
-            )
-                or die($mysqli->error);
-
-            $sqlQuery = $sql_code->fetch_assoc();
-            if (!empty($sqlQuery['matricula'])) {
-                $matricula = $sqlQuery['matricula'];
-            } else {
-                $erro = "Número de matrícula inválido!";
-            }
-        }
-    }
-}
-if (empty($senha)) {
-    $erro = "Preencha o campo SENHA corretamente";
-} else {
-    $senha = password_hash($senha, PASSWORD_DEFAULT);
-}
-if (empty($telefone)) {
-    $telefone = limpar_texto($telefone);
-    if (strlen($telefone) != 11) {
-        $erro = "O telefone deve ser preenchido no padrão: (11) 98888-8888";
-    }
-}
-
-if ($erro) {
-    echo "<p><b>$erro</b></p>";
-} else {
-    $sql_code = "INSERT INTO tbl_usuario 
-        (nome_usuario, email_usuario, cpf_usuario, senha_usuario, telefone_usuario, endereco_usuario, perfil) 
-        VALUES ('$nome', '$email', '$cpf', '$senha', '$telefone', '$endereco', 0);";
-
-    $query = $mysqli->query($sql_code) or die($mysqli->error);
-
-    if ($query) {
-        echo "<p><b>Cadastro realizado com sucesso!</b></p>";
-
-        $inserirAluno = $mysqli->query("INSERT INTO tbl_aluno (nome, cpf, matricula, email, data_cadastro)
-            VALUES ('$nome', '$cpf', '$matricula', '$email', NOW())");
-        unset($_POST);
-    } else {
-        echo "Erro!, Usuário não cadastrado!</b></p>";
-    }
-}
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -88,6 +6,9 @@ if ($erro) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/cadastroFuncionario.css">
+    <link rel="stylesheet" href="./assets/css/cadastrar_aluno.css">
+    <script src="https://kit.fontawesome.com/a91a789ba3.js" crossorigin="anonymous"></script>
+
     <title>Cadastro - Lib Manager</title>
 </head>
 
@@ -105,45 +26,189 @@ if ($erro) {
     </header>
 
     <!-- Corpo ( container) -->
-    <main>
-        <section>
-            <h2>Lib Manager - Formulário de Cadastro de Aluno</h2>
-            <p>
-                Informe seus dados cadastrais:
-            </p>
-            <form action="" method="post">
-                <p>
-                    <label for="">Nome: </label>
-                    <input type="text" name="nome" placeholder="Digite seu nome">
-                </p>
-                <p>
-                    <label for="">Email: </label>
-                    <input type="text" name="email" placeholder="seueamail@dominio.com">
-                </p>
-                <p>
-                    <label for="">CPF: </label>
-                    <input type="text" name="cpf" placeholder="000.000.000-00">
-                </p>
-                <p>
-                    <label for="">Matrícula: </label>
-                    <input type="text" name="matricula" placeholder="202301">
-                </p>
-                <p>
-                    <label for="">Senha: </label>
-                    <input type="password" name="senha" placeholder="*********">
-                </p>
-                <p>
-                    <label for="">Telefone: </label>
-                    <input type="text" name="telefone" placeholder="DDD + NÚMERO">
-                </p>
-                <p>
-                    <label for="">Endereço: </label>
-                    <textarea name="endereco" id="" cols="20" rows="5"></textarea>
-                </p>
-                <button type="submit" name="click">Cadastrar</button>
-            </form>
-        </section>
+    <main class="container">
+        <form class="container-formulario" action="" method="post">
+            <h1 class="titulo">Lib Manager</h1>
+            <h2 class="sub-titulo">REGISTRAR</h2>
+
+            <div class="container-cpfMatricula">
+                <!-- CPF -->
+                <div class="container_input">
+                    <input id="txtCpf" oninput="mascaraCPF(this)" type="text" name="cpf" placeholder="CPF">
+                </div>
+                <!-- Matricula-->
+                <div class="container_input">
+                    <input id="txtMatricula" type="number" name="matricula" placeholder="Matricula">
+                </div>
+            </div>
+            <!-- Nome -->
+            <div class="container_input">
+                <input id="txtNome" type="text" name="nome" placeholder="Digite seu nome">
+            </div>
+            <!-- Email -->
+
+            <div class="container_input">
+                <input id="txtEmail" type="text" name="email" placeholder="seueamail@dominio.com">
+            </div>
+            <div class="container-foneSenha">
+                <!-- FONE -->
+                <div class="container_input">
+                    <input id="txtfone" oninput="mascaraFone(this)" type="fone" name="telefone" placeholder="DDD + NÚMERO">
+                </div>
+                <!-- SENHA-->
+                <div class="container_input">
+                    <input id="password" type="password" name="senha">
+                    <i id="olho" class="fa-solid fa-eye olho"></i>
+                </div>
+            </div>
+            <!-- Endereço -->
+            <div class="container_input">
+                <input id="txtEndereco" type="text" name="endereco" placeholder="Endereço completo">
+            </div>
+
+            <button class="btn-entrar" type="submit" name="click">Cadastrar</button>
+        </form>
     </main>
+
+    <!-- JS Local -->
+    <script src="./assets/js/login.js"></script>
+
+    <!-- jquery-3.6.3.js -->
+    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
+    <!-- notify.min.js -->
+    <script src="./assets/js/notify.min.js"></script>
 </body>
 
 </html>
+
+<?php
+include('conexao.php');
+
+$erro = false;
+if (count($_POST) > 1) {
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $cpf = $_POST['cpf'];
+    $matricula = $_POST['matricula'];
+    $senha = $_POST['senha'];
+    $telefone = $_POST['telefone'];
+    $endereco = $_POST['endereco'];
+
+    if (empty($nome)) {
+?>
+        <script type="text/javascript">
+            $.notify("Preencha o nome", "warn");
+        </script>
+    <?php
+        //$erro = "Preencha o nome";
+    }
+    if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    ?>
+        <script type="text/javascript">
+            $.notify("Preencha o campo Email", "warn");
+        </script>
+    <?php
+        //$erro = "Preencha o campo Email";
+    }
+    if (empty($cpf) || strlen($cpf) < 14 || !validarCpf($cpf)) {
+    ?>
+        <script type="text/javascript">
+            $.notify("CPF Inválido. Tente novamente!", "error");
+        </script>
+    <?php
+        //$erro = "CPF Inválido. Tente novamente!";
+    }
+    $sqlQuery = false;
+    if (empty($matricula || strlen($matricula) > 25)) {
+    ?>
+        <script type="text/javascript">
+            $.notify("Informe uma matrícula válida!", "warn");
+        </script>
+        <?php
+        //$erro = "Infomre uma matrícula válida!";
+    } else {
+        $sql_code = $mysqli->query("SELECT matricula FROM tbl_aluno WHERE matricula = '$matricula' LIMIT 1");
+        $sqlQuery = $sql_code->fetch_assoc();
+
+        if (!empty($sqlQuery['matricula'])) {
+        ?>
+            <script type="text/javascript">
+                $.notify("Essa matrícula já está vinculada a um aluno registrado no sistema!", "error");
+            </script>
+    <?php
+            //$erro = "Essa matrícula já está vinculada a um aluno registrado no sistema!";
+        } else {
+            $sql_code = $mysqli->query(
+                "SELECT matricula FROM tbl_matriculas WHERE matricula = '$matricula' LIMIT 1"
+            )
+                or die($mysqli->error);
+
+            $sqlQuery = $sql_code->fetch_assoc();
+            if (!empty($sqlQuery['matricula'])) {
+                $matricula = $sqlQuery['matricula'];
+            } else {
+                $erro = "Número de matrícula inválido!";
+            }
+        }
+    }
+}
+if (empty($senha)) {
+    ?>
+    <script type="text/javascript">
+        $.notify("Preencha o campo SENHA corretamente", "warn");
+    </script>
+    <?php
+    //$erro = "Preencha o campo SENHA corretamente";
+} else {
+    $senha = password_hash($senha, PASSWORD_DEFAULT);
+}
+if (empty($telefone)) {
+    $telefone = limpar_texto($telefone);
+    if (strlen($telefone) != 11) {
+    ?>
+        <script type="text/javascript">
+            $.notify("O telefone deve ser preenchido no padrão: (11) 98888-8888", "warn");
+        </script>
+    <?php
+        //$erro = "O telefone deve ser preenchido no padrão: (11) 98888-8888";
+    }
+}
+
+if ($erro) {
+    ?>
+    <script type="text/javascript">
+        $.notify("Número de matrícula inválido!", "error");
+    </script>
+    <?php
+    //echo "<p><b>$erro</b></p>";
+} else {
+    $sql_code = "INSERT INTO tbl_usuario 
+        (nome_usuario, email_usuario, cpf_usuario, senha_usuario, telefone_usuario, endereco_usuario, perfil) 
+        VALUES ('$nome', '$email', '$cpf', '$senha', '$telefone', '$endereco', 0);";
+
+    $query = $mysqli->query($sql_code) or die($mysqli->error);
+
+    if ($query) {
+    ?>
+        <script type="text/javascript">
+            $.notify("Cadastro realizado com sucesso!", "success");
+        </script>
+    <?php
+        //echo "<p><b>Cadastro realizado com sucesso!</b></p>";
+
+        $inserirAluno = $mysqli->query("INSERT INTO tbl_aluno (nome, cpf, matricula, email, data_cadastro)
+            VALUES ('$nome', '$cpf', '$matricula', '$email', NOW())");
+        unset($_POST);
+    } else {
+    ?>
+        <script type="text/javascript">
+            $.notify("Erro!, Usuário não cadastrado!", "error");
+        </script>
+<?php
+        //echo "Erro!, Usuário não cadastrado!</b></p>";
+    }
+}
+
+
+?>
